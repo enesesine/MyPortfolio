7817c67bbf9da795544dc25e29fab9019d011149
enesesine <eneslucker@gmail.com>
2025-02-25 18:04:18 +0100

    scroll behaviour smooth + button 'lets talk' connected with contact me section'

--- my-portfolio/src/app/about-me/about-me.component.ts
+++ my-portfolio/src/app/about-me/about-me.component.ts
@@ -5,8 +5,13 @@ import { Component } from '@angular/core';
   standalone: true,
   imports: [],
   templateUrl: './about-me.component.html',
-  styleUrl: './about-me.component.scss'
+  styleUrl: './about-me.component.scss',
 })
 export class AboutMeComponent {
-
+  scrollToContact(): void {
+    const contactSection = document.getElementById('contact');
+    if (contactSection) {
+      contactSection.scrollIntoView({ behavior: 'smooth' });
+    }
+  }
 }
