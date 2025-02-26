8f2375fd632cbe1fb0f6131dc8ce5369408fceb3
enesesine <eneslucker@gmail.com>
2025-02-26 02:57:15 +0100

    Effects added

--- my-portfolio/src/app/skill-set/skill-set.component.ts
+++ my-portfolio/src/app/skill-set/skill-set.component.ts
@@ -1,10 +1,17 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-skill-set',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './skill-set.component.html',
   styleUrl: './skill-set.component.scss',
 })
-export class SkillSetComponent {}
+export class SkillSetComponent {
+  pulled: boolean = false; // Zustand: false => Pull.png, true => PulledImage.png
+
+  togglePull() {
+    this.pulled = !this.pulled;
+  }
+}
